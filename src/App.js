7138bfa5533d94a9e8eec8cbb850500214b2682a
GotChun <qkreogo1@naver.com>
2024-12-27 18:32:09 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import MakePlanner from './planner/makePlanner/MakePlanner';
-import {useState, useEffect} from 'react';
-import React from 'react';
-import StartPage from './planner/StartPage/StartPage';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'; // BrowserRouter를 올바르게 임포트
+import LoginForm from './login/components/LoginForm';
 
 function App() {
-
   return (
-    <BrowserRouter> {/* Router -> BrowserRouter로 수정 */}
-      <Link to="/planner/makePlanner">플래너 만들기</Link>
+    <>
+      <h1>LOGIN FORM</h1>
+      <LoginForm/>
 
-      <Routes>
-        <Route path="/planner/makePlanner" element={<MakePlanner />} />
-      </Routes>
-    </BrowserRouter>
+
+    </>
   );
 }
 
---
+++ src/user/User.jsx
@@ -0,0 +1,9 @@
+
+const User = ()=>{
+
+    return (
+        <div>
+            <h1>USER PAGE 입니당 ~ </h1>
+        </div>
+    )
+}
