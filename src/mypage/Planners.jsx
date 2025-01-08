b58bd1636b6c11bb31b26f26c4b3a1d42c54ac15
wlscksals <cksals8731@naver.com>
2025-01-08 13:49:40 +0900

    .

--- src/mypage/Planners.jsx
+++ src/mypage/Planners.jsx
@@ -67,6 +67,21 @@ const Planners = () => {
     navigate(`/destination?plannerID=${plannerID}`, { state: { plannerItem } });
   };
 
+  const handlelikePlannerClick = (plannerID) => {
+    if (!likedPlanners || likedPlanners.length === 0) {
+      console.warn("플래너 데이터가 초기화되지 않았습니다.");
+      return;
+    }
+
+    const plannerItem = likedPlanners.find((item) => item.plannerID === plannerID);
+    if (!plannerItem) {
+      console.warn("PlannerID에 해당하는 플래너가 없습니다.");
+      return;
+    }
+
+    navigate(`/destination?plannerID=${plannerID}`, { state: { plannerItem } });
+  };
+
   // 로딩 상태
   if (loading || plannersLoading || likeLoading) return <div>로딩 중...</div>;
 
@@ -111,7 +126,7 @@ const Planners = () => {
               <li
                 key={likedPlanners.plannerID || index}
                 className="planner-item"
-                onClick={() => handlePlannerClick(likedPlanners.plannerID)}
+                onClick={() => handlelikePlannerClick(likedPlanners.plannerID)}
               >
                 <h4>{likedPlanners.plannerTitle}</h4>
                 <p>PlannerID: {likedPlanners.plannerID}</p>
